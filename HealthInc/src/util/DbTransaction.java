328adf2c8f8fe9e8fced9c8b172a26746228deae
Suraj Kumar <Suraj Kumar@DESKTOP-61TV1E0>
2016-09-18 03:50:25 +0530

    DbTransaction

---
+++ HealthInc/src/util/DbTransaction.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbTransaction 
+{
+	private static Connection connection=null;
+	private static String url="jdbc:oracle:thin:@localhost:1521:xe";
+	private static String user="system";
+	private static String password="pratyush";
+
+	public Connection getConnection() 
+	{
+		try 
+		{
+			closeConnection(connection);
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection(url,user,password);
+		} 		
+		catch (SQLException e) 
+		{
+		e.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) 
+		{
+		e.printStackTrace();
+		}
+		return connection;
+	
+	}
+    public static void closeConnection(Connection connection)
+    {
+		 
+		if(connection!=null){
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		}	
+	}
+}
